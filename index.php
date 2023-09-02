b8f0ce60d209bc300a1cdaf716a74d806ed69d25
Sourav Kumar Saha <94992938+NULLcodehub@users.noreply.github.com>
2023-09-02 09:52:14 +0600

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <div class="banner">
         <video autoplay loop muted>
         <source src="images/bg.mp4" type="video/mp4">
-                 Your browser does not support the video tag.
+                 
         </video>
                 
             </div>
