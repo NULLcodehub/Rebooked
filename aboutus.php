1016f8dde2b14c04b96789b820181d714480acbd
Sourav Kumar Saha <94992938+NULLcodehub@users.noreply.github.com>
2023-09-02 09:50:06 +0600

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -49,7 +49,7 @@
         </div>
     </div>
 
-    <?php include "footer.php" ?> <!-- Include your footer here -->
+    <?php include "footer.php" ?> 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
